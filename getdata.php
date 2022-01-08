ffede298968824beb65c77705fcd3fbe847e1458
Allen <allen@cyfordtechnologies.com>
2022-01-08 17:47:09 -0500

    1st upload

---
+++ getdata.php
@@ -0,0 +1,20 @@
+<?php
+$file= file_get_contents("testfile.txt");
+//var_dump($file);
+$pattern = "/\[(.*?)]/m";
+preg_match_all($pattern, $file, $matches, PREG_SET_ORDER, 0);
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Defiant Test</title>
+</head>
+
+<body>
+    <?php
+    var_dump($matches);
+    ?>
+</body>
+</html>
